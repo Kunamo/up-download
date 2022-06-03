992852c5bf697b63b920e5b9b51503470697bad8
Kawabongo <98980709+real-kunnun@users.noreply.github.com>
2022-06-03 14:16:04 +0200

    Finished frontend + Assignement PDF

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //JS Part of App.js
+    const [error, setError] = useState("noerror")
+    const [message, setMessage] = useState("nomessage")
+    const [file, setFile] = useState("nofile")
+
+    const onFileChange = (event) => {
+        setFile(event.target.files[0])
+    }
+
+    const uploadFile = (event) => {
+        setError("")
+        setMessage("")
+
+        let data = new FormData()
+        data.append('file', file)
+        data.append('name', file.name)
+
+        fetch('http://localhost:8080/api/files', {
+            method: 'POST', body: data
+        }).then(response => {
+            setError('')
+            setMessage('Sucessfully uploaded file');
+        }).catch(err => {
+            setError(err)
+        })
+    }
+
+    return (//HTML Part
+        <div className="Upload">
+            <h2>Upload a file</h2>
+            <h4 style={{color: 'red'}}>{error}</h4>
+            <h4 style={{color: 'green'}}>{message}</h4>
+            <input onChange={onFileChange} type="file"></input>
+            <button onClick={uploadFile}>Upload</button>
+        </div>);
 }
 
 export default App;
